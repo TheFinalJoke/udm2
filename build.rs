fd25c2ece1a7213dd5fe69d5c117d64201799906
Nick Shorter <nickshorter888@gmail.com>
2023-12-05 03:32:48 +0000

    Set up Dev Container and started first proto

---
+++ build.rs
@@ -0,0 +1,14 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let protos = &[
+        "protos/test/test.proto"
+    ];
+    // compile protocol buffer using protoc
+    tonic_build::configure()
+        .build_server(true)
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(
+            protos,
+            &["protos"],
+        )?;
+    Ok(())
+}
---
+++ sys/README.md
@@ -0,0 +1,6 @@
+# Sys and Systems Utilities
+
+## Purpose
+
+This folder is build to have low level performance gains as this will interact with the kernel and hardware
+
