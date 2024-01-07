fcb22d5fb822e8a7f96c82ea7316b37b32302e5f
Your Name <you@example.com>
2024-01-07 15:37:13 +0000

    Fixed some linter errors

---
+++ scripts/install_protolint.sh
@@ -0,0 +1,7 @@
+PROTOLINT_VERSION="0.47.4"
+
+packageurl="https://github.com/yoheimuta/protolint/releases/download/v$PROTOLINT_VERSION/protolint_$PROTOLINT_VERSION_linux_amd64.tar.gz"
+
+wget -O /tmp/ $packageurl
+
+tar -zxvf /tmp/
