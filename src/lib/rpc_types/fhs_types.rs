68f0675f46ce236011d04d90739671f410f2f6a7
Nick Shorter <nickshorter888@gmail.com>
2024-03-02 00:34:15 +0000

    updated with tests

--- src/lib/rpc_types/fhs_types.rs
+++ src/lib/rpc_types/fhs_types.rs
@@ -31,3 +31,21 @@ impl GenQueries for FluidRegulator {
             .to_owned()
     }
 }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use sea_query::PostgresQueryBuilder;
+
+    #[test]
+    fn test_gen_insert_query() {
+        let fr = FluidRegulator {
+            fr_id: Some(1),
+            gpio_pin: Some(23),
+            regulator_type: Some(RegulatorType::Tap.into()),
+        };
+        let query = fr.gen_insert_query().to_string(PostgresQueryBuilder);
+        let expected_query = r#"INSERT INTO "FluidRegulation" ("gpio_pin", "regulator_type") VALUES (23, 3) RETURNING "fr_id""#.to_string();
+        assert_eq!(query, expected_query)
+    }
+}
